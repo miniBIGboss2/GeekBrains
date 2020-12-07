31afadcc86a7a7f3ef0e81e0f3a689384e525890
miniBIGboss <miniBIGboss2@gmail.com>
2020-12-07 16:40:54 +0300

    Написал второе задание

---
+++ LessonThreeHomework/Program.cs
@@ -0,0 +1,14 @@
+﻿//Костяков Дмитрий
+
+using LessonThreeHomework.Tasks;
+
+namespace LessonThreeHomework
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            SumOfOddPositiveNumbersExtention.StartProgram(); //Задание №2
+        }
+    }
+}
