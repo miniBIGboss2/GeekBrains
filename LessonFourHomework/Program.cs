98722b6d71fd1e60963dec28dc5ba224f5233b85
miniBIGboss <miniBIGboss2@gmail.com>
2020-12-14 19:56:58 +0300

    Сделал первое задание

---
+++ LessonFourHomework/Program.cs
@@ -0,0 +1,18 @@
+﻿//Костяков Дмитрий
+
+using System;
+using LessonFourHomework.Tasks;
+
+namespace LessonFourHomework
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            DivisiblePairs.StartProgram(); //Задание №1
+            
+            Console.ReadKey();
+
+        }
+    }
+}
