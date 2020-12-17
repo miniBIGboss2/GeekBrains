d7a93948ef3d9b7502f039535e449852b0b02eab
miniBIGboss <miniBIGboss2@gmail.com>
2020-12-17 12:37:30 +0300

    Сделал первое задание пятого урока

---
+++ LessonFiveHomework/Program.cs
@@ -0,0 +1,17 @@
+﻿//Костяков Дмитрий
+
+using System;
+using LessonFiveHomework.Tasks;
+
+namespace LessonFiveHomework
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            CorrectLoginRegex.StartProgram();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ LessonFiveHomework/Tasks/CorrectLoginRegex.cs
@@ -0,0 +1,49 @@
+/*
+    Создать программу, которая будет проверять корректность ввода логина. Корректным логином будет строка от 2 до 10 
+    символов, содержащая только буквы латинского алфавита или цифры, при этом цифра не может быть первой:
+    
+        а) Без использования регулярных выражений;
+        б) С использованием регулярных выражений.
+ */
+
+using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace LessonFiveHomework.Tasks
+{
+    public class CorrectLoginRegex
+    {
+        public static void StartProgram()
+        {
+            const string rightLoginText = "Корректный логин.";
+            const string wrongLoginText = "Не корректный логин.";
+            string userInput;
+            bool isLoginCorrect;
+            
+            Console.Write("Программа проверяет логин на корректность.\nВведите свой логин: ");
+
+            try
+            { 
+                userInput = string.Concat((Console.ReadLine() ?? string.Empty).Where(c=> !char.IsWhiteSpace(c)));
+            }
+            catch (ArgumentNullException)
+            {
+                throw new ArgumentNullException("userInput", "Значение Логин не может быть пустым.");
+            }
+
+            // isLoginCorrect = NotRegexWay(userInput);
+            isLoginCorrect = RegexWay(userInput);
+
+            Console.WriteLine(isLoginCorrect ? rightLoginText : wrongLoginText);
+        }
+
+        private static bool NotRegexWay(string input)
+        {
+            if (input == null || input.Length < 2 || input.Length > 10) return false;
+            return char.IsLetter(input[0]) && input.All(char.IsLetterOrDigit);
+        }
+
+        private static bool RegexWay(string input) => Regex.IsMatch(input, @"^[^0-9][a-zA-Z0-9]+$");
+    }
+}
