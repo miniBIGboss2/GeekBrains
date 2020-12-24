f68d499187e4992c68279a74f868f2fe62d9614f
miniBIGboss <miniBIGboss2@gmail.com>
2020-12-24 18:50:21 +0300

    Сделал второе задание а и б

--- LessonSixHomework/Program.cs
+++ LessonSixHomework/Program.cs
@@ -9,7 +9,7 @@ namespace LessonSixHomework
     {
         public static void Main()
         {
-            MinFunction.StartProgram(); // Задание №1
+            MinFunction.StartProgram(); // Задание №1 и №2
 
             Console.ReadKey();
         }
--- LessonSixHomework/Tasks/MinFunction.cs
+++ LessonSixHomework/Tasks/MinFunction.cs
@@ -1,7 +1,15 @@
 /*
+    Задание №1
     Изменить программу вывода функции так, чтобы можно было передавать функции типа double (double,double). 
-    
     Продемонстрировать работу на функции с функцией a*x^2 и функцией a*sin(x).
+    
+    Задание №2
+    Модифицировать программу нахождения минимума функции так, чтобы можно было передавать функцию в виде делегата.
+        а) Сделайте меню с различными функциями и предоставьте пользователю выбор, для какой функции 
+            и на каком отрезке находить минимум.
+        б) Используйте массив (или список) делегатов, в котором хранятся различные функции.
+        в) Переделайте функцию Load, чтобы она возвращала массив считанных значений. 
+             Пусть она возвращает минимум через параметр.
  */
 
 using System;
@@ -11,41 +19,72 @@ namespace LessonSixHomework.Tasks
 {
     public static class MinFunction
     {
+        private static double CustomFuncOne(double x, double a) => x * x - 50 * x + 10;
+        private static double CustomFuncTwo(double x, double a) => a * x * x;
+        private static double Sinus(double x, double a) => a * Math.Sin(x);
+
+        private static readonly Func<double,double,double>[] Functions = {
+            CustomFuncOne,
+            CustomFuncTwo,
+            Sinus
+        };
+        
         public static void StartProgram()
         {
-            Func<double, double, double> delegate1 = CustomFuncOne;
-            SaveFunc("data.bin", -100, 0,100, delegate1);
-            Console.WriteLine(Load("data.bin"));
+            try
+            {
+                var index = Menu();
 
-            Func<double, double, double> delegate2 = CustomFuncTwo;
-            SaveFunc("data.bin", 3, -2,100, delegate2);
-            Console.WriteLine(Load("data.bin"));
+                Console.Write("Напишите значение с которого начнется отрезок: ");
+                var x = GetDouble();
 
-            Func<double, double, double> delegate3 = Sinus;
-            SaveFunc("data.bin", -100, 100,100, delegate3);
-            Console.WriteLine(Load("data.bin"));
+                Console.Write("Напишите значение 'a': ");
+                var a = GetDouble();
+
+                Console.Write("Напишите значение на котором закончится отрезок: ");
+                var endOfRange = GetDouble();
+                
+                SaveFunc("data.bin", x, a, endOfRange, Functions[index -1]);
+                Console.WriteLine($"Минимальное число на отрезке: {LoadFunc("data.bin")}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
         
-        private static void SaveFunc(string fileName, double x, double a, double endOfRange, Func<double, double, double> function)
+        private static double GetDouble() => double.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+        private static int Menu()
+        {
+            var numb = 1;
+
+            foreach (var t in Functions)
+            {
+                Console.WriteLine($"{numb}) {t.Method}");
+                numb++;
+            }
+
+            Console.Write("Выберите функцию из списка: ");
+
+            return int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+        }
+
+        private static void SaveFunc(string fileName, double x, double a, double endOfRange, Func<double,double,double> func)
         {
             var step = 0.5;
-            
+
             using (var fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
             using (var bw = new BinaryWriter(fs))
                 while (x <= endOfRange)
                 {
-                    bw.Write(function(x, a));
+                    bw.Write(func(x,a));
                     x += step;
                 }
         }
 
-        private static double CustomFuncOne(double x, double a) => x * x - 50 * x + 10;
-
-        private static double CustomFuncTwo(double x, double a) => a * x * x;
-
-        private static double Sinus(double x, double a) => a * Math.Sin(x);
-
-        private static double Load(string fileName)
+        private static double LoadFunc(string fileName)
         {
             var min = double.MaxValue;
 
